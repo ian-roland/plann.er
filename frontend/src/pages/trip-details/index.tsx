46685054a12f8cdea7282fad220bdd6fad82d0fe
Ian Roland <ianrolandnascimento@gmail.com>
2024-07-14 20:02:10 -0300

    Adicionando info ao index.tsx de trip-details

--- frontend/src/pages/trip-details/index.tsx
+++ frontend/src/pages/trip-details/index.tsx
@@ -1,3 +1,34 @@
+import { Calendar, MapPin, Settings2 } from "lucide-react";
+
 export function TripDetailsPage() {
-    return <h1>1</h1>
+    return (
+        <div className="max-w-6xl px-6 py-10 mx-auto">
+            <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
+                <div className="flex items-center gap-2">
+                    <MapPin className="size-5 text-zinc-400" />
+                    <span className="text-zinc-100">
+                        Sumaré, Brasil
+                    </span>
+                </div>
+
+                <div className="flex items-center gap-5">
+                    <div className="flex items-center gap-2">
+                        <Calendar className="size-5 text-zinc-400" />
+                        <span className="text-zinc-100">
+                            17 a 23 de Agosto
+                        </span>
+                    </div>
+
+                    <div className="w-px h-6 bg-zinc-800" />
+
+                    <button className="bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700">
+                        Alterar local/data
+                        <Settings2 className="size-5" />
+                    </button>
+
+
+                </div>
+            </div>
+        </div>
+    )
 }
